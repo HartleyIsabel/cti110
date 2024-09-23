c02c78a8a040c93fc83fd76378e150de52ae0020
HartleyIsabel <hartleyi5165@student.faytechcc.edu>
2024-09-22 22:57:36 -0400

    Add files via upload

---
+++ P1LAB1_HartleyIsabel.py
@@ -0,0 +1,12 @@
+#Isabel Hartley
+#22 September 2024
+#P1LAB1
+#The project is to create a code in IDLE that allows the user to enter their name. 
+
+
+
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name;', end= ' ')
+last_name = input()
+print('Hello', first_name last_name', 'Welcome to CTI-110')
