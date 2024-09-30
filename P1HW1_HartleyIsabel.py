16915841c4d95ed178d9c4850b6c80277c057fc7
HartleyIsabel <hartleyi5165@student.faytechcc.edu>
2024-09-29 22:55:12 -0400

    Add files via upload

---
+++ P1HW1_HartleyIsabel.py
@@ -0,0 +1,18 @@
+'''
+Isabel Hartley
+29 Sept 2024
+P1HW1
+Writing a python program while using mathematical expressions
+'''
+
+$base^exponent=result$
+base=int(input("Enter the base:"))
+exponent=int(input("Enter the exponent;"))
+result=base**exponent
+print(f"{base}^{exponent}={result}")
+int1=(int(inpur("Enter the first integer:"))
+int2=int(input("Enter the second integer:"))
+int3=int(inpupt("Enter the third integer:"))
+result=(int1+int2)-int3
+pring(f"{(int1+in2)}-{int3}={result}")
+      
